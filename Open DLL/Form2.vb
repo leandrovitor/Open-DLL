30d49006d7332a0d6159f176ed680fc17727915c
leandrovitor <leandrovitorlelis@gmail.com>
2024-01-23 00:32:02 -0300

    Add files via upload

---
+++ Open DLL/Form2.vb
@@ -0,0 +1,8 @@
+﻿Imports Open
+Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Form1 As New Form1
+        Form1.Show()
+        Me.Hide()
+    End Sub
+End Class
